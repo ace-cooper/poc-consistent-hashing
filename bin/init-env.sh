72701a4b9c668dab431bb790e8e60073dc85f89a
Hugo Fagundes <hugo.c.fagundes@gmail.com>
2023-05-07 22:44:40 -0300

    chore: bashes

---
+++ bin/init-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$_PRELUDE_" != "true" ]; then
+  source "${PWD}/bin/prelude.sh"
+fi
+
+if [[ ! -f "$ROOT_FOLDER/.env" ]]; then
+    echo "export DB_PORT=26257" >> $ROOT_FOLDER/.env
+    echo "export DATABASE_URL=\"postgresql://root:xxxxxx@localhost:26257/defaultdb\"" >> $ROOT_FOLDER/.env
+    echo "export CACHE_URL=[\"redis://localhost:6379\",\"redis://localhost:6380\",\"redis://localhost:6381\"]" >> $ROOT_FOLDER/.env
+fi
+
+if [[ ! -f "$ROOT_FOLDER/.env.test" ]]; then
+    echo "export DB_PORT=26258" >> $ROOT_FOLDER/.env.test
+    echo "export DATABASE_URL=\"postgresql://root:xxxxxx@localhost:26258/defaultdb\"" >> $ROOT_FOLDER/.env.test
+    echo "export CACHE_URL=[\"redis://localhost:6479\",\"redis://localhost:6480\",\"redis://localhost:6481\"]" >> $ROOT_FOLDER/.env.test
+fi
---
+++ bin/prelude.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export _PRELUDE_=true
+export ROOT_FOLDER=$(pwd)
+export SRC_FOLDER="${ROOT_FOLDER}/src"
